61433689138c70a0215889f0da00e911c76e5d1b
Aleksandrs Zaslavskis <sahsariga111@gmail.com>
2020-09-15 22:30:40 +0100

    ConfingParserV2

--- src/core/config_parser/config_parse.rs
+++ src/core/config_parser/config_parse.rs
@@ -1,52 +1,90 @@
+/*
+encoding=utf-8
 
+[pid-config]
+# roll PID settings
+p_pid_roll= 1
+i_pid_roll=2
+d_pid_roll=3
+max_roll= 400
+
+# Yaw PID settings
+p_pid_yaw=1
+i_pid_yaw=2
+d_pid_yaw=3
+max_yaw=400
+# Yaw PID settings
+
+p_pid_pitch=1
+i_pid_pitch=2
+d_pid_pitch = 3
+# Pitch PID settings
+
+
+
+
+ */
 
 
 extern crate ini;
 use ini::Ini;
-use std::str::FromStr;
+use std::num::ParseIntError;
 
+//Converts &str to i32
+fn parse(first_number_str: &str) -> Result<i32, ParseIntError> {
+    let value = first_number_str.parse::<i32>()?;
+    Ok(value)
+}
 
-
-
-
-
-pub fn read_config_pid() -> Vec<[i32; 4]> {
+//Takes from config data parser it and return in i32 format
+pub fn pid_parse(section:String, p_str:String, i_str:String, d_str:String, max:String) -> [i32; 4] {
     let conf = Ini::load_from_file("./src/config/core.ini").unwrap();
-    let pids_config = conf.section(Some("pid-config")).unwrap();
-    let roll = pids_config.get("p_pid_roll");
-    let yaw = pids_config.get("p_pid_yaw");
-    let pitch = pids_config.get("p_pid_pitch");
-
-    let mut pid_roll: [i32; 4] = [0; 4];
-    pid_roll[0]=i32::from_str(roll.as_deref().unwrap_or("p_pid_roll")).unwrap();
-    pid_roll[1]=i32::from_str(roll.as_deref().unwrap_or("i_pid_roll")).unwrap();
-    pid_roll[2]=i32::from_str(roll.as_deref().unwrap_or("d_pid_roll")).unwrap();
-    pid_roll[3]=i32::from_str(roll.as_deref().unwrap_or("max_roll")).unwrap();
-    let mut pid_yaw: [i32; 4] = [0; 4];
-    pid_yaw[0]=i32::from_str(yaw.as_deref().unwrap_or("p_pid_yaw")).unwrap();
-    pid_yaw[1]=i32::from_str(yaw.as_deref().unwrap_or("i_pid_yaw")).unwrap();
-    pid_yaw[2]=i32::from_str(yaw.as_deref().unwrap_or("d_pid_yaw")).unwrap();
-    pid_yaw[3]=i32::from_str(yaw.as_deref().unwrap_or("max_yaw")).unwrap();
-    let mut pid_pitch: [i32; 4] = [0; 4];
-    pid_pitch[0]=i32::from_str(pitch.as_deref().unwrap_or("p_pid_pitch")).unwrap();
-    pid_pitch[1]=i32::from_str(pitch.as_deref().unwrap_or("i_pid_pitch")).unwrap();
-    pid_pitch[2]=i32::from_str(pitch.as_deref().unwrap_or("d_pid_pitch")).unwrap();
-    pid_pitch[3]=i32::from_str(pitch.as_deref().unwrap_or("max_pitch")).unwrap();
-
-    let mut values = Vec::new();
-    values.push(pid_roll);
-    values.push(pid_yaw);
-    values.push(pid_pitch);
-    return values;
-
-
-
-
+    let pid_configuration = conf.section(Some(section)).unwrap();
+    let  p_pid = (pid_configuration.get(p_str)).unwrap();
+    let  i_pid=pid_configuration.get(i_str).unwrap();
+    let  d_pid= pid_configuration.get(d_str).unwrap();
+    let  max= pid_configuration.get(max).unwrap();
+    let i32_p_pid= parse(p_pid).unwrap();
+    let i32_i_pid= parse(i_pid).unwrap();
+    let i32_d_pid= parse(d_pid).unwrap();
+    let i32_max= parse(max).unwrap();
+    let mut pid: [i32; 4] = [0; 4];
+    pid[0]=i32_p_pid;
+    pid[1]=i32_i_pid;
+    pid[2]=i32_d_pid;
+    pid[3]=i32_max;
+    return pid;
 
 
 
 
 }
+
+ pub fn get_pids() -> Vec<[i32; 4]> {
+    let roll = pid_parse("pid-config".parse().unwrap(), "p_pid_roll".parse().unwrap(), "i_pid_roll".parse().unwrap(), "d_pid_roll".parse().unwrap(), "max_roll".parse().unwrap());
+    let yaw = pid_parse("pid-config".parse().unwrap(), "p_pid_yaw".parse().unwrap(), "i_pid_yaw".parse().unwrap(), "d_pid_yaw".parse().unwrap(), "max_yaw".parse().unwrap());
+    let pitch = pid_parse("pid-config".parse().unwrap(), "p_pid_pitch".parse().unwrap(), "i_pid_pitch".parse().unwrap(), "d_pid_pitch".parse().unwrap(), "max_pitch".parse().unwrap());
+    let mut values = Vec::new();
+    values.push(roll);
+    values.push(yaw);
+    values.push(pitch);
+    return values;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-#[path = "core/logger/simple_logger.rs"] mod simple_logger;
-#[path = "core/config_parser/config_parse.rs"] mod config_parser;
+use crate::config_parser:: get_pids;
+
+#[path = "core/logger/simple_logger.rs"]
+mod simple_logger;
+#[path = "core/config_parser/config_parse.rs"]
+mod config_parser;
+
+
+
+
 fn main() {
-    simple_logger::logger(1, false, "NO ERRORS".parse().unwrap());
-   println!("{}",config_parser::read_config_pid().get(0).unwrap()[0]);
+    simple_logger::logger(1, false, "SOFTWARE RUN".parse().unwrap());
+    simple_logger::logger(1, false, "CONFIG READ&PARSE".parse().unwrap());
+    let pids_values = get_pids();
+    println!("{}",pids_values.get(0).unwrap()[0]);
+    simple_logger::logger(1, false, "CONFIG READ&PARSE=OK".parse().unwrap());
 }
