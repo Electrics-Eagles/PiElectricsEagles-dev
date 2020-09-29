71bb7a8541c060c48cefbfe9fffe9d0bc8aa3e4d
misha-dig <55639759+misha-dig@users.noreply.github.com>
2020-09-29 23:12:16 +0100

    Edited funcion to return struct

--- src/config_parse.rs
+++ src/config_parse.rs
@@ -71,6 +71,12 @@ pub struct EscMotors {
     pub amount: u8
 }
 
+pub struct PID {
+    pub p: i32,
+    pub i: i32,
+    pub d: i32
+}
+
 pub fn esc_config_parser() -> EscMotors {
     let conf = Ini::load_from_file("./src/config/core.ini").unwrap();
     let esc_config = conf.section(Some("esc-config")).unwrap();
@@ -113,7 +119,7 @@ pub fn sbus_receiver_conifg() -> SbusConfig {
     };
     return sbus;
 }
-
+/*
 //Takes from config data parser it and return in i32 format
 pub fn pid_parse(section: String, p_str: String, i_str: String, d_str: String, max: String) -> [i32; 4] {
     let conf = Ini::load_from_file("./src/config/core.ini").unwrap();
@@ -133,7 +139,30 @@ pub fn pid_parse(section: String, p_str: String, i_str: String, d_str: String, m
     pid[3] = i32_max;
     return pid;
 }
-
+*/
+pub fn pid_parse(section: String, p_str: String, i_str: String, d_str: String, max: String) -> PID {
+    let conf = Ini::load_from_file("./src/config/core.ini").unwrap();
+    let pid_configuration = conf.section(Some(section)).unwrap();
+    let p_pid = (pid_configuration.get(p_str)).unwrap();
+    let i_pid = pid_configuration.get(i_str).unwrap();
+    let d_pid = pid_configuration.get(d_str).unwrap();
+    let max = pid_configuration.get(max).unwrap();
+    let i32_p_pid = parse(p_pid).unwrap();
+    let i32_i_pid = parse(i_pid).unwrap();
+    let i32_d_pid = parse(d_pid).unwrap();
+    let i32_max = parse(max).unwrap();
+    /*let mut pid: [i32; 4] = [0; 4];
+    pid[0] = i32_p_pid;
+    pid[1] = i32_i_pid;
+    pid[2] = i32_d_pid;Steps
+    return pid;*/ // OLD STYLE CODE
+    let pid_config:PID = PID {
+        p: i32_p_pid,
+        i: i32_i_pid,
+        d: i32_d_pid
+    };
+    return pid_config
+}
 pub fn get_pids() -> Vec<[i32; 4]> {
     let roll = pid_parse("pid-config".parse().unwrap(), "p_pid_roll".parse().unwrap(), "i_pid_roll".parse().unwrap(), "d_pid_roll".parse().unwrap(), "max_roll".parse().unwrap());
     let yaw = pid_parse("pid-config".parse().unwrap(), "p_pid_yaw".parse().unwrap(), "i_pid_yaw".parse().unwrap(), "d_pid_yaw".parse().unwrap(), "max_yaw".parse().unwrap());
