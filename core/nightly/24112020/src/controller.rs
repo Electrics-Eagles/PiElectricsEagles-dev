fad0282794d30e50cf0c1990b5c96ba64fdb576e
misha-dig <55639759+misha-dig@users.noreply.github.com>
2020-11-26 22:56:48 +0000

    Update controller.rs

--- core/nightly/24112020/src/controller.rs
+++ core/nightly/24112020/src/controller.rs
@@ -1,56 +1,44 @@
 const FREQ_CONST: i32 = 83; // Const to set freq of signalC
 const PRESCALER: u8 = 83;
 
-use crate::config_parse::mpu_config_parser;
 use linux_embedded_hal::{Delay, I2cdev};
 use pwm_pca9685::{Address, Channel, Pca9685};
-
-use crate::simple_logger;
 
 pub struct Controller {
     pwm: Pca9685<I2cdev>,
 }
 impl Controller {
-    // initialize pmw i2c controller
-    pub fn new() -> Controller {
-            simple_logger::logger(1, true, "Controller created ".parse().unwrap());
-        let mpu6050_conifg = mpu_config_parser();
-        let dev = I2cdev::new(mpu6050_conifg.port).unwrap();
-        let mut pwm_mod: Pca9685<I2cdev> = Pca9685::new(dev, Address::default()).unwrap();
-        pwm_mod.set_prescale(PRESCALER).unwrap(); // you need to set a correct prescaler
-        pwm_mod.set_channel_on_off(Channel::C0, 1000, 2000).unwrap();
-        pwm_mod.set_channel_on_off(Channel::C1, 1000, 2000).unwrap();
-        pwm_mod.set_channel_on_off(Channel::C2, 1000, 2000).unwrap();
-        pwm_mod.set_channel_on_off(Channel::C3, 1000, 2000).unwrap();
-           simple_logger::logger(1, true, "set channels ".parse().unwrap());
-        pwm_mod.enable().unwrap();
-         simple_logger::logger(1, true, "turn on channels ".parse().unwrap());
-        Controller { pwm: pwm_mod }
-    }
-    // turn motor by selecting channel and speed
-    pub fn turn_motor(&mut self, addr_of_ch: Channel, value: u16) {
-        self.pwm.set_channel_on(addr_of_ch, value).unwrap();
-           simple_logger::logger(1, true, " TURN ON MOTORS ".parse().unwrap());
-    }
-    // stop to rotate all motor
-    pub fn stop_all_motors(&mut self) {
-        self.pwm.set_channel_full_off(Channel::C0).unwrap();
-        self.pwm.set_channel_full_off(Channel::C1).unwrap();
-        self.pwm.set_channel_full_off(Channel::C2).unwrap();
-        self.pwm.set_channel_full_off(Channel::C3).unwrap();
-           simple_logger::logger(1, true, " TURN OFF MOTORS ".parse().unwrap());
-    }
-    pub fn set_throttle_external_pwm(&mut self, ch1: u16, ch2: u16, ch3: u16, ch4: u16) {
-         simple_logger::logger(1, true, "CHANNELS LIST ".parse().unwrap());
-simple_logger::logger(1, true, ch1.to_string().parse().unwrap());
-simple_logger::logger(1, true, ch2.to_string().parse().unwrap());
-simple_logger::logger(1, true, ch3.to_string().parse().unwrap());
-simple_logger::logger(1, true, ch4.to_string().parse().unwrap());
+   pub fn new(path_dev_i2c: String) -> Controller {
+    let dev = I2cdev::new(path_dev_i2c).unwrap();
+    let mut pwm_mod: Pca9685<I2cdev>  = Pca9685::new(dev, Address::default()).unwrap();
+    pwm_mod.set_prescale(PRESCALER).unwrap(); // you need to set a correct prescaler 
+    pwm_mod.enable().unwrap();
+    Controller { pwm: pwm_mod }
+   }
+   pub fn turn_motor(&mut self, addr_of_ch:Channel, value_micro: u16)
+   {
+       let mut value_pwm = value_micro * (4095 / 20000);
+       self.pwm.set_channel_on(addr_of_ch, value_pwm).unwrap();
+   }
+   pub fn stop_all_motors(&mut self)
+   {
+    self.pwm.set_channel_full_off(Channel::C0).unwrap();
+    self.pwm.set_channel_full_off(Channel::C1).unwrap();
+    self.pwm.set_channel_full_off(Channel::C2).unwrap();
+    self.pwm.set_channel_full_off(Channel::C3).unwrap();
+   }
+   pub fn set_throttle_external_pwm(&mut self, ch1:u16, ch2:u16, ch3:u16, ch4:u16) 
+   {
+       let mut value_pwm_ch1 = ch1 * (4095 / 20000);
+       self.pwm.set_channel_on(Channel::C0, value_pwm_ch1).unwrap();
+       
+       let mut value_pwm_ch2 = ch2 * (4095 / 20000);
+       self.pwm.set_channel_on(Channel::C1, value_pwm_ch2).unwrap();
 
+       let mut value_pwm_ch3 = ch3 * (4095 / 20000);
+       self.pwm.set_channel_on(Channel::C2, value_pwm_ch3).unwrap();
 
-        self.pwm.set_channel_on(Channel::C0, ch1).unwrap();
-        self.pwm.set_channel_on(Channel::C1, ch2).unwrap();
-        self.pwm.set_channel_on(Channel::C2, ch3).unwrap();
-        self.pwm.set_channel_on(Channel::C3, ch4).unwrap();
-    }
+       let mut value_pwm_ch4 = ch4 * (4095 / 20000);  
+       self.pwm.set_channel_on(Channel::C3, value_pwm_ch4).unwrap();
+   }
 }
