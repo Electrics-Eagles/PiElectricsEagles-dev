9c1a6c8a72d98b02bb2e71e16d36ce8286a0edeb
misha-dig <55639759+misha-dig@users.noreply.github.com>
2020-12-07 22:14:32 +0000

    Update controller.rs
    -Added calibration esc function before starting to work esc
    -Fixed ircorrect convert value 
    -Add testing function (for running same pulse in all channel esc) for checking how esc works
    Important! You must call a function of calibration esc after initialize esc class before start to run motor. By using method calibrate_esc();. Otherwise, esc doesn't work without calibration!

--- core/nightly/24112020/src/controller.rs
+++ core/nightly/24112020/src/controller.rs
@@ -1,28 +1,25 @@
-//config found on osciloscope 
-// min 1000 us value 205  
-// max 2000us value 410 
-// prescaller 120 -119 
-// diffeence 119 
-
+//config found on osciloscope
+// min 1000 us value 205
+// max 2000us value 410
+// prescaller 120 -119
+// diffeence 119
 
 const PRESCALER: u8 = 120;
-
-use crate::config_parse::mpu_config_parser;
 use linux_embedded_hal::I2cdev;
 use pwm_pca9685::{Address, Channel, Pca9685};
+use std::{thread, time};
 pub struct Controller {
     pwm: Pca9685<I2cdev>,
 }
 
-pub fn map(x: i32, in_min: i32, in_max: i32, out_min: i32, out_max: i32) -> i32 {
+pub fn map(x: i64, in_min: i64, in_max: i64, out_min: i64, out_max: i64) -> i64 {
     let val = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     val
 }
 
 impl Controller {
     pub fn new() -> Controller {
-        let mpu6050_conifg = mpu_config_parser();
-        let dev = I2cdev::new(mpu6050_conifg.port).unwrap();
+        let dev = I2cdev::new("/dev/i2c-1").unwrap();
         let mut pwm_mod: Pca9685<I2cdev> = Pca9685::new(dev, Address::default()).unwrap();
         pwm_mod.set_prescale(PRESCALER).unwrap(); // you need to set a correct prescaler
         pwm_mod.set_channel_on(Channel::C0, 0).unwrap();
@@ -32,76 +29,64 @@ impl Controller {
         pwm_mod.enable().unwrap();
         Controller { pwm: pwm_mod }
     }
-    /*
-    pub fn turn_motor(&mut self, addr_of_ch: Channel, value_micro: u16) {
-        let value_pwm = value_micro * (4095 / 20000);
-        self.pwm.set_channel_on(addr_of_ch, value_pwm).unwrap();
-    }
-    pub fn stop_all_motors(&mut self) {
-        self.pwm.set_channel_full_off(Channel::C0).unwrap();
-        self.pwm.set_channel_full_off(Channel::C1).unwrap();
-        self.pwm.set_channel_full_off(Channel::C2).unwrap();
-        self.pwm.set_channel_full_off(Channel::C3).unwrap();
-    }
-    */
-    /*
     pub fn set_throttle_external_pwm(&mut self, ch1: u16, ch2: u16, ch3: u16, ch4: u16) {
-        let mut value_pwm_ch1 = ch1 * (4095 / 20000);
-        self.pwm.set_channel_on(Channel::C0, value_pwm_ch1).unwrap();
+        let value_pwm_ch1 = map(ch1 as i64, 0, 20000, 0, 4095) as u16;
+        println!("From :{}, OUT: {}", value_pwm_ch1, ch1);
+        self.pwm
+            .set_channel_on_off(Channel::C0, 0, value_pwm_ch1)
+            .unwrap();
 
-        let mut value_pwm_ch2 = ch2 * (4095 / 20000);
-        self.pwm.set_channel_on(Channel::C1, value_pwm_ch2).unwrap();
+        let value_pwm_ch2 = map(ch2 as i64, 0, 20000, 0, 4095) as u16;
+        self.pwm
+            .set_channel_on_off(Channel::C1, 0, value_pwm_ch2)
+            .unwrap();
 
-        let mut value_pwm_ch3 = ch3 * (4095 / 20000);
-        self.pwm.set_channel_on(Channel::C2, value_pwm_ch3).unwrap();
+        let value_pwm_ch3 = map(ch3 as i64, 0, 20000, 0, 4095) as u16;
+        self.pwm
+            .set_channel_on_off(Channel::C2, 0, value_pwm_ch3)
+            .unwrap();
 
-        let mut value_pwm_ch4 = ch4 * (4095 / 20000);
-        self.pwm.set_channel_on(Channel::C3, value_pwm_ch4).unwrap();
+        let value_pwm_ch4 = map(ch4 as i64, 0, 20000, 0, 4095) as u16;
+        self.pwm
+            .set_channel_on_off(Channel::C3, 0, value_pwm_ch4)
+            .unwrap();
+        self.pwm.enable().unwrap();
     }
-    */
-/*
-    pub fn set_throttle_external_pwm(&mut self, ch1: u16, ch2: u16, ch3: u16, ch4: u16) {
-        let value_pwm_ch1 = map(ch1 as i32, 205, 410, 0, 4095) as u16;
-        self.pwm.set_channel_on(Channel::C0, value_pwm_ch1).unwrap();
-
-        let value_pwm_ch2 = map(ch2 as i32, 205, 410, 0, 4095) as u16;
-        self.pwm.set_channel_on(Channel::C1, value_pwm_ch2).unwrap();
-
-        let value_pwm_ch3 = map(ch3 as i32, 205, 410, 0, 4095) as u16;
-        self.pwm.set_channel_on(Channel::C2, value_pwm_ch3).unwrap();
-
-        let value_pwm_ch4 = map(ch4 as i32, 205, 410, 0, 4095) as u16;
-        self.pwm.set_channel_on(Channel::C3, value_pwm_ch4).unwrap();
+    pub fn test_esc(&mut self, pwm_duty: u16) {
+        self.pwm
+            .set_channel_on_off(Channel::C0, 0, pwm_duty)
+            .unwrap();
+        self.pwm.enable();
     }
-    
-    let value_pwm_ch1 = map(ch1 as i32, 1000, 2000, 205 410) as u16;
-        println!("{:?}",value_pwm_ch1 );
-        self.pwm.set_channel_on(Channel::C0, value_pwm_ch1).unwrap();
+    pub fn calibrate_esc(&mut self) {
+        let value_pwm_max = map(2000, 0, 20000, 0, 4095) as u16;
+        self.pwm
+            .set_channel_on_off(Channel::C0, 0, value_pwm_max)
+            .unwrap();
+        self.pwm
+            .set_channel_on_off(Channel::C1, 0, value_pwm_max)
+            .unwrap();
+        self.pwm
+            .set_channel_on_off(Channel::C2, 0, value_pwm_max)
+            .unwrap();
+        self.pwm
+            .set_channel_on_off(Channel::C3, 0, value_pwm_max)
+            .unwrap();
+        thread::sleep(time::Duration::from_millis(3000));
 
-        let value_pwm_ch2 = map(ch2 as i32, 1000, 2000, 205 410) as u16;
-        self.pwm.set_channel_on(Channel::C1, value_pwm_ch2).unwrap();
-
-        let value_pwm_ch3 = map(ch3 as i32, 1000, 2000, 205 410) as u16;
-        self.pwm.set_channel_on(Channel::C2, value_pwm_ch3).unwrap();
-
-        let value_pwm_ch4 = map(ch4 as i32, 1000, 2000, 205 410) as u16;
-        self.pwm.set_channel_on(Channel::C3, value_pwm_ch4).unwrap();
+        let value_pwm_min = map(1000, 0, 20000, 0, 4095) as u16;
+        self.pwm
+            .set_channel_on_off(Channel::C0, 0, value_pwm_min)
+            .unwrap();
+        self.pwm
+            .set_channel_on_off(Channel::C1, 0, value_pwm_min)
+            .unwrap();
+        self.pwm
+            .set_channel_on_off(Channel::C2, 0, value_pwm_min)
+            .unwrap();
+        self.pwm
+            .set_channel_on_off(Channel::C3, 0, value_pwm_min)
+            .unwrap();
+        thread::sleep(time::Duration::from_millis(3000));
+    }
 }
-
-*/
-
- pub fn set_throttle_external_pwm(&mut self, ch1: u16, ch2: u16, ch3: u16, ch4: u16) {
-        let value_pwm_ch1 = map(ch1 as i32, 1000, 2000, 205 ,410) as u16;
-        println!("{:?}",value_pwm_ch1 );
-        self.pwm.set_channel_on(Channel::C0, value_pwm_ch1).unwrap();
-
-        let value_pwm_ch2 = map(ch2 as i32, 1000, 2000, 205 ,410) as u16;
-        self.pwm.set_channel_on(Channel::C1, value_pwm_ch2).unwrap();
-
-        let value_pwm_ch3 = map(ch3 as i32, 1000, 2000, 205 ,410) as u16;
-        self.pwm.set_channel_on(Channel::C2, value_pwm_ch3).unwrap();
-
-        let value_pwm_ch4 = map(ch4 as i32, 1000, 2000, 205 ,410) as u16;
-        self.pwm.set_channel_on(Channel::C3, value_pwm_ch4).unwrap();
-    }
-    }
