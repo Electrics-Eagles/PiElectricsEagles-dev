8febdc874bb6696d6e3e2a29007a3a4a9e77d387
Alex Zaslavskis <sahsariga111@gmail.com>
2020-12-30 17:49:22 +0200

    buil

--- gyro_getter/main.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-import random 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return str(random.randint(0,1000))+","+str(random.randint(0,1000))+","+str(random.randint(0,1000))
