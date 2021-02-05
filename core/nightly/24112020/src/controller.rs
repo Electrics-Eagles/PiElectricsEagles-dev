6281219a2b596fe27b76ef4a14f437d41e539996
misha-dig <55639759+misha-dig@users.noreply.github.com>
2021-02-05 19:58:26 +0200

    Update controller.rs

--- core/nightly/24112020/src/controller.rs
+++ core/nightly/24112020/src/controller.rs
@@ -1,10 +1,30 @@
-//config found on osciloscope
+//
+//
+// Alex Zaslavkis (Electrics Eagles) 2021
+//
+// ---------------- Controller Driver --------------------
+//
+// Simple software level that esc launch for drone. 
+//
+// typical usage :
+//    new(); // create struct of esc driver 
+//    calibrate_esc(); // auto-calibrate for esc motor before launch. This function is mandatory to use before launching esc
+//    set_throttle_external_pwm(ch1: u16, ch2: u16, ch3: u16, ch4: u16); // set throttle to how speed run each esc with selectable value 1000-2000
+//    test_esc(pwm_duty: u16); // check esc how run each motor with value of pwm duty same
+//
+//    let mut controller = Controller::new(); // Initialize Contriller flieds
+//    controller.calibrate_esc(); // Calibrate esc
+//    controller.set_throttle_external_pwm(1050, 1050, 1050, 1050, 1050); // Launch esc 
+//     
+//
+// Enjoy
+// config found on osciloscope
 // min 1000 us value 205
 // max 2000us value 410
 // prescaller 120 -119
 // diffeence 119
 
-const PRESCALER: u8 = 120;
+const PRESCALER: u8 = 120; // Clock prescaler of MCU raspberry pi zero 
 use crate::config_parse::config_parser;
 use linux_embedded_hal::I2cdev;
 use pwm_pca9685::{Address, Channel, Pca9685};
@@ -17,8 +37,32 @@ pub fn map(x: i64, in_min: i64, in_max: i64, out_min: i64, out_max: i64) -> i64 
     let val = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     val
 }
-
+/// It is a Controller object
+/// Controller class (crate)
+/// 
+/// *****Already added to loggics file. Be careful. Editing code can break stability of devices.*****
+/// # Examples
+///
+/// ```
+/// use crate::controller::Controller;
+/// let mut controller = Controller::new();
+/// ```
+///
 impl Controller {
+    /// Returns Controller object
+    ///
+    /// # Arguments
+    ///
+    /// No arguments required
+    ///
+    /// # Examples
+    /// **** Already added to loggics file. Be careful. Editing code can break stability of devices. *****
+    ///
+    /// ```
+    /// use crate::controller::Controller;
+    /// let mut controller = Controller::new();
+    /// ```
+    ///
     pub fn new() -> Controller {
         let mut config = config_parser::new();
         let dev = I2cdev::new(config.esc_config_parser().port).unwrap();
@@ -32,6 +76,34 @@ impl Controller {
         
         Controller { pwm: pwm_mod }
     }
+    /// Launch esc motors by selected pwm duty on each motor
+    /// 
+    /// # Arguments
+    /// 
+    /// ch1 - channel 1
+    /// 
+    /// ch2 - channel 2
+    /// 
+    /// ch3 - channel 3
+    /// 
+    /// ch4 - channel 4
+    /// 
+    /// All values of channels are range from 1000 - 2000 us
+    /// 
+    /// 1000 - slowest speed of running esc
+    /// 
+    /// 2000 - maximun speed of running esc 
+    /// 
+    /// 
+    /// # Examples
+    /// **** Already added to loggics file. Be careful. Editing code can break stability of devices. *****
+    ///
+    /// ```
+    /// use crate::controller::Controller;
+    /// let mut controller = Controller::new();
+    /// controller.set_throttle_external_pwm(1050, 1050, 1050, 1050, 1050);
+    /// ```
+    ///
     pub fn set_throttle_external_pwm(&mut self, ch1: u16, ch2: u16, ch3: u16, ch4: u16) {
         let value_pwm_ch1 = map(ch1 as i64, 0, 20000, 0, 4095) as u16;
         println!("From :{}, OUT: {}", value_pwm_ch1, ch1);
@@ -55,12 +127,48 @@ impl Controller {
             .unwrap();
         self.pwm.enable().unwrap();
     }
+    /// Testing launching esc motors by selected pwm duty for all motor. 
+    /// 
+    /// Value of pmw duty is same for all motors
+    /// 
+    /// # Arguments
+    /// pwm_duty - speed to test how launch esc motors
+    /// 
+    /// 1000 - slowest speed of running esc
+    /// 
+    /// 2000 - maximun speed of running esc 
+    /// 
+    /// 
+    /// # Examples
+    /// **** Already added to loggics file. Be careful. Editing code can break stability of devices. *****
+    ///
+    /// ```
+    /// use crate::controller::Controller;
+    /// let mut controller = Controller::new();
+    /// controller.test_esc(1500);
+    /// ```
+    ///
     pub fn test_esc(&mut self, pwm_duty: u16) {
         self.pwm
             .set_channel_on_off(Channel::C0, 0, pwm_duty)
             .unwrap();
         self.pwm.enable();
     }
+    /// Calibration all esc motors
+    ///
+    /// # Arguments
+    ///
+    /// No arguments required
+    ///
+    /// # Examples
+    /// **** Already added to loggics file. Be careful. Editing code can break stability of devices. *****
+    ///
+    /// ```
+    /// use crate::controller::Controller;
+    /// let mut controller = Controller::new();
+    /// controller.calibrate_esc();
+    /// ```
+    ///
     pub fn calibrate_esc(&mut self) {
         let value_pwm_max = map(2000, 0, 20000, 0, 4095) as u16;
         self.pwm
