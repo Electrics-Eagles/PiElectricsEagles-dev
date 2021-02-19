79dd61943d65959bbe6723802ea06535b55d4c76
misha-dig <55639759+misha-dig@users.noreply.github.com>
2021-02-19 16:20:26 +0200

    Update controller.rs

--- dev/src/controller.rs
+++ dev/src/controller.rs
@@ -5,10 +5,11 @@
 // ---------------- Controller Driver --------------------
 //
 // Simple software level that esc launch for drone. 
-//
+// 
+// 
 // typical usage :
 //    new(); // create struct of esc driver 
-//    calibrate_esc(); // auto-calibrate for esc motor before launch. This function is mandatory to use before launching esc
+//    calibrate_esc(); // auto-calibrate for esc motor before launch. This function is mandatory to use before launching esc.
 //    set_throttle_external_pwm(ch1: u16, ch2: u16, ch3: u16, ch4: u16); // set throttle to how speed run each esc with selectable value 1000-2000
 //    test_esc(pwm_duty: u16); // check esc how run each motor with value of pwm duty same
 //
@@ -16,38 +17,52 @@
 //    controller.calibrate_esc(); // Calibrate esc
 //    controller.set_throttle_external_pwm(1050, 1050, 1050, 1050, 1050); // Launch esc 
 //     
+//    config found on osciloscope
+//    min 1000 us value 205
+//    max 2000us value 410
+//    prescaller 120 -119
+//    diffeence 119
 //
 // Enjoy
-// config found on osciloscope
-// min 1000 us value 205
-// max 2000us value 410
-// prescaller 120 -119
-// diffeence 119
 
-const PRESCALER: u8 = 120; // Clock prescaler of MCU raspberry pi zero 
+/// Clock prescaler of MCU raspberry pi zero
+const PRESCALER: u8 = 120;
+ 
 use crate::config_parse::config_parser;
 use linux_embedded_hal::I2cdev;
 use pwm_pca9685::{Address, Channel, Pca9685};
 use std::{thread, time};
-pub struct Controller {
-    pwm: Pca9685<I2cdev>,
-}
 
 pub fn map(x: i64, in_min: i64, in_max: i64, out_min: i64, out_max: i64) -> i64 {
     let val = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     val
 }
+
 /// It is a Controller object
 /// Controller class (crate)
+
+///
+/// Made by : Misha Zaslavkis (Electrics Eagles) 2021
+///
 /// 
-/// *****Already added to loggics file. Be careful. Editing code can break stability of devices.*****
+/// 
+/// Simple software level that esc launch for drone.
+/// Esc motor lauching with selected pwm and test how rotate motors.
+/// 
+/// *Already added to loggics file. Be careful. Editing code can break stability of devices.*
 /// # Examples
 ///
 /// ```
 /// use crate::controller::Controller;
-/// let mut controller = Controller::new();
+/// let mut controller = Controller::new(); // Initialize class of esc driver
+/// controller.calibrate_esc(); // Calibrate esc 
+/// controller.set_throttle_external_pwm(1050, 1050, 1050, 1050, 1050); // Run esc's motor with selected pusle width
 /// ```
 ///
+pub struct Controller {
+    pwm: Pca9685<I2cdev>,
+}
+
 impl Controller {
     /// Returns Controller object
     ///
@@ -56,7 +71,7 @@ impl Controller {
     /// No arguments required
     ///
     /// # Examples
-    /// **** Already added to loggics file. Be careful. Editing code can break stability of devices. *****
+    /// ***** Already added to loggics file. Be careful. Editing code can break stability of devices. *****
     ///
     /// ```
     /// use crate::controller::Controller;
@@ -100,8 +115,9 @@ impl Controller {
     ///
     /// ```
     /// use crate::controller::Controller;
-    /// let mut controller = Controller::new();
-    /// controller.set_throttle_external_pwm(1050, 1050, 1050, 1050, 1050);
+    /// let mut controller = Controller::new(); // create object of Controller
+    /// controller.calibrate_esc(); // Calibrate esc. This function is mandatory to use before starting run esc
+    /// controller.set_throttle_external_pwm(1050, 1050, 1050, 1050, 1050); // Set specifity pusle of pwm generate esc driver
     /// ```
     ///
     pub fn set_throttle_external_pwm(&mut self, ch1: u16, ch2: u16, ch3: u16, ch4: u16) {
@@ -131,6 +147,8 @@ impl Controller {
     /// 
     /// Value of pmw duty is same for all motors
     /// 
+    ///  To avoid doesn't work esc, use function of calibarating esc. Call function ```calibrate_esc();``` before launch esc.
+    /// 
     /// # Arguments
     /// pwm_duty - speed to test how launch esc motors
     /// 
@@ -145,6 +163,7 @@ impl Controller {
     /// ```
     /// use crate::controller::Controller;
     /// let mut controller = Controller::new();
+    /// controller.calibrate_esc();
     /// controller.test_esc(1500);
     /// ```
     ///
@@ -155,7 +174,10 @@ impl Controller {
         self.pwm.enable();
     }
     /// Calibration all esc motors
-    ///
+    /// 
+    /// This fuction give teachering the esc controller what range of output throttle input it should respond to.
+    /// A function give range of pulse 1000-2000 to teaching the esc controller.
+    /// Before usage esc, you must use this function to works property esc. This function is mandatory!
     /// # Arguments
     ///
     /// No arguments required
