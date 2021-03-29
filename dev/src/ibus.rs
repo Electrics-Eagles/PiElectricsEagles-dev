85034e7754a73ca4ac8295c4cbf7fb1371e2ef9e
misha-dig <55639759+misha-dig@users.noreply.github.com>
2021-03-29 23:38:08 +0100

    Update ibus.rs

--- dev/src/ibus.rs
+++ dev/src/ibus.rs
@@ -19,11 +19,10 @@ use crate::config_parse::config_parser;
 use rppal::uart::Queue::Both;
 use rppal::uart::{Parity, Uart}; // crate for uart reading
 use std::iter::FromIterator; // std libary for convert into string via vec with char
+use std::thread;
 use std::time::Duration;
 use std::vec::Vec;
 
-use crate::simple_logger;
-use simple_logger::*;
 /// value for default value of channels when IBUS didn't get signal transmistion
 static mut value_before: [u16; 6] = [1000, 1000, 1000, 1000, 1000, 1000];
 /// It is a struct for getting data from ibus receiver
@@ -82,9 +81,9 @@ impl ibus_receiver {
     ///
     pub fn new() -> ibus_receiver {
         let mut uart_def: Uart = Uart::new(115_200, Parity::None, 8, 1).unwrap();
-        simple_logger::write_log(LevelOfLog::INFO, "UART CREATED".parse().unwrap());
-        uart_def.set_read_mode(40, Duration::new(1, 7)).unwrap();
-        simple_logger::write_log(LevelOfLog::INFO, "UART MODE SET".parse().unwrap());
+        //simple_logger::write_log(LevelOfLog::INFO, "UART CREATED".parse().unwrap());
+        uart_def.set_read_mode(32, Duration::new(1, 7)).unwrap();
+        //simple_logger::write_log(LevelOfLog::INFO, "UART MODE SET".parse().unwrap());
         ibus_receiver { uart_mod: uart_def }
     }
 
@@ -120,46 +119,54 @@ impl ibus_receiver {
             //simple_logger::logger(1, true, std::str::from_utf8(&buffer).unwrap().to_string());
             // encode buffer into hex decimal and convert string
             let input_string_in_hex = hex::encode(buffer);
+            println!("{}", input_string_in_hex);
             // contain chars of hex decimal values by using std::vec
             let input_string_in_char: Vec<char> = input_string_in_hex.chars().collect();
-            // each channel is size two bytes after welcoming two bytes (0x20 and 0x40)
-            if input_string_in_char[0] == '2'
-                && input_string_in_char[1] == '0'
-                && input_string_in_char[2] == '4'
-                && input_string_in_char[3] == '0'
-            {
-                // each two bytes need convert opposite site form second byte of each channel into first byte of each channel
 
-                // simple_logger::logger(1, true, "DATA READED AFTER HEX CONVERTION".parse().unwrap());
-                //simple_logger::logger(1, true, input_string_in_hex.parse().unwrap());
-                // support maxiumun 14 number of channels
-                // starting at 3th and 4th bytes of one reading lenght of ibus, but need set opposite of these bytes that get correctly value of each channel
-                // each new channel new number of char
-                // iterate six times, because we need to have six channels of receivers
-                for x in 0..6 {
-                    // make reorder each two bytes by using calculating position of char with support this rules of convert bytes in this situation
-                    // we get 16bit value of each channel, so we use 4 bytes of characters of each channel
-                    // each new channel, we multiply by 4 of position of bytes, because we use 4 character of each channels
-                    // 1 byte is 2 characters for get value of channel
-                    let ch1_raw_hex = vec![
-                        input_string_in_char[6 + (4 * x)],
-                        input_string_in_char[7 + (4 * x)],
-                        input_string_in_char[4 + (4 * x)],
-                        input_string_in_char[5 + (4 * x)],
-                    ];
+            if input_string_in_char.len() > 0 {
+                // each channel is size two bytes after welcoming two bytes (0x20 and 0x40)
+                if input_string_in_char[0] == '2'
+                    && input_string_in_char[1] == '0'
+                    && input_string_in_char[2] == '4'
+                    && input_string_in_char[3] == '0'
+                {
+                    // each two bytes need convert opposite site form second byte of each channel into first byte of each channel
 
-                    // convert string with real hex value
-                    let str_value = String::from_iter(ch1_raw_hex);
-                    simple_logger::write_log(LevelOfLog::INFO,"Hex with replaced value".parse().unwrap());
-                    simple_logger::write_log(LevelOfLog::INFO, str_value.parse().unwrap());
-                    // convert into u16 from hex string
-                    let value: u16 = u16::from_str_radix(str_value.as_str(), 16).unwrap();
+                    // simple_logger::logger(1, true, "DATA READED AFTER HEX CONVERTION".parse().unwrap());
+                    //simple_logger::logger(1, true, input_string_in_hex.parse().unwrap());
+                    // support maxiumun 14 number of channels
+                    // starting at 3th and 4th bytes of one reading lenght of ibus, but need set opposite of these bytes that get correctly value of each channel
+                    // each new channel new number of char
+                    // iterate six times, because we need to have six channels of receivers
+                    for x in 0..6 {
+                        // make reorder each two bytes by using calculating position of char with support this rules of convert bytes in this situation
+                        // we get 16bit value of each channel, so we use 4 bytes of characters of each channel
+                        // each new channel, we multiply by 4 of position of bytes, because we use 4 character of each channels
+                        // 1 byte is 2 characters for get value of channel
+                        let ch1_raw_hex = vec![
+                            input_string_in_char[6 + (4 * x)],
+                            input_string_in_char[7 + (4 * x)],
+                            input_string_in_char[4 + (4 * x)],
+                            input_string_in_char[5 + (4 * x)],
+                        ];
 
-                    // write a new value of channel into array
-                    data_of_channels[x] = value;
-                }
-                unsafe {
-                    value_before = data_of_channels;
+                        // convert string with real hex value
+                        let str_value = String::from_iter(ch1_raw_hex);
+                        //simple_logger::write_log(LevelOfLog::INFO,"Hex with replaced value".parse().unwrap());
+                        //simple_logger::write_log(LevelOfLog::INFO, str_value.parse().unwrap());
+                        // convert into u16 from hex string
+                        let value: u16 = u16::from_str_radix(str_value.as_str(), 16).unwrap();
+
+                        // write a new value of channel into array
+                        data_of_channels[x] = value;
+                    }
+                    unsafe {
+                        value_before = data_of_channels;
+                    }
+                } else {
+                    unsafe {
+                        data_of_channels = value_before;
+                    }
                 }
             } else {
                 unsafe {
@@ -167,7 +174,10 @@ impl ibus_receiver {
                 }
             }
         }
-        self.uart_mod.flush(Both).expect("error");
+        thread::sleep(Duration::from_millis(1));
+        self.uart_mod
+            .flush(rppal::uart::Queue::Input)
+            .expect("error");
         // return into struct of data of channels from usual array
         //simple_logger::logger(1, true, String::from_utf16(&data_of_channels).unwrap());
         type_of_data_from_channels {
