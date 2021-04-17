bbeecef5d94fae63213bcdc180e7fb1431b8e2d0
Alex Zaslavskis <sahsariga111@gmail.com>
2021-04-18 00:13:04 +0300

    Update main.rs

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -11,23 +11,14 @@ mod clk_driver;
 mod config_parse;
 mod controller;
 mod ibus;
+mod logger;
 mod loggics;
 mod mpu6050;
-mod simple_logger;
 fn version_display() {
     let mut config = config_parser::new();
-
-    simple_logger::write_log(LevelOfLog::INFO, "VERISONS OF MODULES ".parse().unwrap());
-    simple_logger::write_log(LevelOfLog::INFO, logger_verison().parse().unwrap());
-    simple_logger::write_log(
-        LevelOfLog::INFO,
-        config.config_parser_version().parse().unwrap(),
-    );
-    // simple_logger::logger(1, FILE_LOG, sbus_verison().parse().unwrap());
 }
 
 fn main() {
-    init_log(); // initialize for logger
     version_display(); // call function that display software verison
     main_loop();
 }
