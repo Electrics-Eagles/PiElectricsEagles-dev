0924de47dc4e3c66676df63184330dc5cd3335a2
misha-dig <55639759+misha-dig@users.noreply.github.com>
2021-07-27 21:04:20 +0300

    Update main.rs

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -1,11 +1,6 @@
-#[macro_use]
-extern crate lazy_static; // crate for convert from u8 to hex
-extern crate mut_static;
 
-use crate::config_parse::config_parser;
 use loggics::main_loop;
 
-use crate::logger::*;
 
 mod clk_driver;
 mod config_parse;
