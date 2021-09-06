540a9ec7605e5b8becf9a0b02bde468612be8feb
Alex Zaslavskis <sahsariga111@gmail.com>
2021-09-06 16:49:05 +0100

    Removed tools
    Removed  deprecated docs . Cleaned repo

--- tools/filter_test/src/filter.rs
+++
@@ -1,8 +0,0 @@
-static mut result_lpf: f64 = 0.0;
-pub fn low_pass_filter(x: f64, delta_t: f64, filtration_period: f64) -> f64 {
-    unsafe {
-        let mut alpha = delta_t / filtration_period;
-        result_lpf += alpha * (x - result_lpf);
-        return result_lpf;
-    }
-}
