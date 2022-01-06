dc9331fa86812284f6bc33a90fe79d5f153e776f
Alex Zaslavskis <sahsariga111@gmail.com>
2022-01-06 13:06:24 +0200

    Update reversed.c

--- betaflight/reversed.c
+++ betaflight/reversed.c
@@ -1,1 +1,25 @@
-@
+//
+
+
+int main(int argc,char *argv[])
+{
+int FD_YAW = -1; 
+float signYaw;
+
+if(getRcDeflection(FD_YAW) < 0 ) {
+    signYaw=1;
+}
+else {
+     signYaw=-1; 
+}
+
+ printf("%lf\n", signYaw);
+return 0;
+}
+
+int getRcDeflection(int x) {
+return x;
+}
+
+
+
