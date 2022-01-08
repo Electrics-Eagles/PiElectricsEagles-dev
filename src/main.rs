fdab44ca659be3186924b0df40bc9e880cad2b0e
Alex Zaslavskis <sahsariga111@gmail.com>
2022-01-08 03:48:10 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[allow(non_camel_case_types)]
 use loggics::main_loop;
 use testing_hardware::test;
+use test_imu::test_imu;
 mod batterycontroller;
 mod config_parse;
 mod controller;
@@ -11,12 +12,15 @@ mod loggics;
 mod reciver;
 mod testing_hardware;
 mod utils;
+mod test_imu;
+
+
 fn main() {
     let  mut args = std::env::args();
     if args.len() > 0 {
         let mode = args.nth(1).unwrap();
         if mode == "--testing_hardware" { test(); }
-        if mode == "--test_imu" {}
+        if mode == "--test_imu" {test_imu();}
         else {
             panic!("I can`t understand you");
         }
